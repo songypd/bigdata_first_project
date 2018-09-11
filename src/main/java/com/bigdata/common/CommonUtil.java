633eb26e38ab75693f4f98d1fb894af88e974945
songypd <galaxy_songypd@163.com>
2018-09-11 13:46:39 +0800

    16进制转中文

---
+++ src/main/java/com/bigdata/common/CommonUtil.java
@@ -0,0 +1,34 @@
+package com.bigdata.common;
+
+/**
+ * @author yuanpeng.song
+ * @create 2018/9/11
+ * @since 1.0.0
+ */
+public class CommonUtil {
+
+    /**
+     * 将16进制转为中文
+     * @param s
+     * @return
+     */
+    public static String hexStr2Str(String s) {
+        s = s.replace("\\x", "");
+        byte[] baKeyword = new byte[s.length() / 2];
+        for (int i = 0; i < baKeyword.length; i++) {
+            try {
+                baKeyword[i] = (byte) (0xff & Integer.parseInt(s.substring(
+                        i * 2, i * 2 + 2), 16));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            s = new String(baKeyword, "utf-8");// UTF-16le:Not
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return s;
+    }
+
+}
--- src/main/java/com/bigdata/nginx/SendRequest.java
+++ src/main/java/com/bigdata/nginx/SendRequest.java
@@ -1,27 +1,70 @@
 package com.bigdata.nginx;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author yuanpeng.song
  * @create 2018/9/10
  * @since 1.0.0
  */
 public class SendRequest {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         //发送 GET 请求
         String url = "http://node05:80/test.log";
         int num = 0;
-        while (true) {
-            if (num > 100) {
-                break;
-            }
-            String s = HttpRequest.sendGet(url, "key=123&v=456");
-            num++;
-
-        }
-//        System.out.println(s);
+        List<String> params = getParams();
+        params.forEach(e -> {
+            String s = HttpRequest.sendGet(url, e.toString());
+            System.err.println(e.toString());
+        });
+        num++;
 
         //发送 POST 请求
 //        String sr=HttpRequest.sendPost("http://localhost:8080/Home/RequestPostString", "key=123&v=456");
 //        System.out.println(sr);
+    }
+
+
+    private static List<String> getParams() throws Exception {
+        InputStreamReader isr;
+        FileInputStream fis;
+        BufferedReader br;
+        File f_01 = new File("D:\\test\\data\\beike_zz.txt");
+        File f_02 = new File("D:\\test\\data\\beike_bj.csv");
+        fis = new FileInputStream(f_01);
+        isr = new InputStreamReader(fis);
+        br = new BufferedReader(isr);
+        List<String> params = new LinkedList<>();
+        String str;
+        int count = 0;
+        boolean f_01_end = false;
+        while ((str = br.readLine()) != null) {
+            if (count == 0) {
+                count++;
+                continue;
+            }
+//            params.add(str);
+
+        }
+//        f_01_end = true;
+        str = null;
+        count = 0;
+        fis = new FileInputStream(f_02);
+        isr = new InputStreamReader(fis);
+        br = new BufferedReader(isr);
+        while ((str = br.readLine()) != null) {
+            if (count == 0) {
+                count++;
+                continue;
+            }
+            params.add(str);
+        }
+        return params;
     }
 }
