9a4f9c2f23d3558007b1ec6e770aa96f18a0cee2
songypd <songyuanpeng@weilaisihang.com>
2018-09-29 11:58:30 +0800

    commit

---
+++ src/test/java/com/common/common.java
@@ -0,0 +1,25 @@
+package com.common;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author yuanpeng.song
+ * @create 18:50 2018/9/3
+ */
+public class common {
+
+    @Test
+    public void test001(){
+        List<String> str001 = new LinkedList<>();
+        List<Integer> int001 = new LinkedList<>();
+//        test(str001,int001);
+
+    }
+    private void test(List<Object>... args){
+        System.out.println();
+    }
+}
+
